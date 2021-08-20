2e3e87dd2f828def36ab607e21f28e817fbf5c75
babak1368 <davoudi.babak@yahoo.com>
2021-08-21 01:08:32 +0430

    Delete authenticationService/dist directory

--- authenticationService/dist/enum/sampleEnum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sampleEnum = void 0;
-var sampleEnum;
-(function (sampleEnum) {
-})(sampleEnum = exports.sampleEnum || (exports.sampleEnum = {}));
-// All the enums will be the folder that hosts this file
-//# sourceMappingURL=sampleEnum.js.map
--- authenticationService/dist/messageHandler/core/baseHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseHandler {
-}
-exports.default = BaseHandler;
-//# sourceMappingURL=baseHandler.js.map
